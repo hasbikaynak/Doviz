14e7c8bf8248084ec08f6238727186f3c30519d5
M-Hasbi <CEM@Olaf.com>
2021-03-24 13:37:52 +0100

    connect SQL, download json nugget package and create ParaBirimiListesi method

--- Core/DatabaseLogicLayer.cs
+++ Core/DatabaseLogicLayer.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core
 {
-    public class DatabaseLogicLayer:BaseClass
+    public class DatabaseLogicLayer : BaseClass //baseclassimizdan kalittik
     {
+        SqlConnection con; //oncelikle gerekli sql connection formatlarimizi tanimliyoruz.
+        SqlCommand cmd;
+        SqlDataReader reader;
+        public DatabaseLogicLayer()
+        {
+            con = new SqlConnection(@"Data Source = OLAF\SQLEXPRESS; Initial Catalog = PhoneBook; Integrated Security = True"); // ctor icerisinde sql baglatimizi kuruyoruz
+        }
+
+        public void BaglantiIslemleri() //baglanti islemleri isimli bir metod olusturup acik olan baglantimizi kapatabiliyoruz veya kapali olan baglantimizi acabiliyoruz.
+        {
+            if (con.State==System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+            else
+            {
+                con.Open();
+            }
+        }
+        public SqlDataReader ParaBirimiListesi()
+        {
+            TryCatchKullan(() => 
+            {
+                cmd = new SqlCommand("Select * from ParaBirimi", con);
+                BaglantiIslemleri();
+            });
+            return cmd.ExecuteReader();
+        }
+
     }
 }
