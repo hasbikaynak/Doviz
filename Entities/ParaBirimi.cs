c3b7e67c3882768eb85994e38b67f2f40b6eee84
M-Hasbi <CEM@Olaf.com>
2021-03-24 12:52:44 +0100

    add Kur,KurGecmisi,ParaBirimi classes

---
+++ Entities/ParaBirimi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class ParaBirimi
+    {
+        public Guid ID { get; set; }
+        public Guid ParaBirimiID{ get; set; }
+        public string Code{ get; set; }
+        public string Tanim { get; set; }
+        public decimal UyariLimit { get; set; }
+        
+    }
+}
